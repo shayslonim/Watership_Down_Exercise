e581ac47ff2f923677f8cc9670377e7b358c0203
Moshe Halperin <57508613+moyshih@users.noreply.github.com>
2020-10-27 20:14:09 +0200

    Update Program.cs

--- Watership_Down_Exercise/Program.cs
+++ Watership_Down_Exercise/Program.cs
@@ -4,21 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Watership_Down_Exercise
-{
-    class Program
-    {
-        //The main program
-        static void Main(string[] args)
-        {
-            List<Bunny> bunnies = new List<Bunny>();
-
-            /* Create 5 bunnies */
-            for (int i = 0; i < 5; i++)
-            {
-                Bunny newBunny = BunnyCreator.CreateBunny();
-                bunnies.Add(newBunny);
-            }
-        }
-    }
-}
+ namespace Watership_Down_Exercise
+ {
+     class Program
+     {
+         //The main program
+         static void Main(string[] args)
+         {
+             List<Bunny> bunnies = new List<Bunny>();
+ 
+             /* Create 5 bunnies */
+             for (int i = 0; i < 5; i++)
+             {
+                 Bunny newBunny = BunnyCreator.CreateBunny();
+                 bunnies.Add(newBunny);
+             }
+         }
+     }
+ }

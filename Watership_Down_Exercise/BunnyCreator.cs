98f3d437b555d52a53933ca03fde8e0b615135d8
Moshe Halperin <57508613+moyshih@users.noreply.github.com>
2020-10-27 20:13:48 +0200

    Update BunnyCreator.cs

--- Watership_Down_Exercise/BunnyCreator.cs
+++ Watership_Down_Exercise/BunnyCreator.cs
@@ -5,42 +5,42 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Watership_Down_Exercise
-{
-    //The BunnyCreator class is used to create bunnies
-    static class BunnyCreator
-    {
-        private static readonly Random random = new Random();
-        public static Bunny CreateBunny()
-        {
-            Bunny bunny;
-            Bunny.Color randomColor;
-            //Get a random number between 1 and 4
-            int randomNumber = random.Next(5);
-
-            //Generate a random color
-            switch (randomNumber)
-            {
-                case 1:
-                    randomColor = Bunny.Color.Black;
-                    break;
-                case 2:
-                    randomColor = Bunny.Color.Brown;
-                    break;
-                case 3:
-                    randomColor = Bunny.Color.Spotted;
-                    break;
-                case 4:
-                default:
-                    randomColor = Bunny.Color.White;
-                    break;
-            }
-            //Create the bunny with the random color
-            bunny = new Bunny(randomColor);
-
-            //[Male/Female] [Black/Brown/Spotted/White] Bunny [Name] Was born!
-            Console.WriteLine("{0} {1} Bunny {2} Was born!", bunny.getSex(), bunny.getColor(), bunny.GetName());
-            return new Bunny(randomColor);
-        }
-    }
-}
+ namespace Watership_Down_Exercise
+ {
+     //The BunnyCreator class is used to create bunnies
+     static class BunnyCreator
+     {
+         private static readonly Random random = new Random();
+         public static Bunny CreateBunny()
+         {
+             Bunny bunny;
+             Bunny.Color randomColor;
+             //Get a random number between 1 and 4
+             int randomNumber = random.Next(5);
+ 
+             //Generate a random color
+             switch (randomNumber)
+             {
+                 case 1:
+                     randomColor = Bunny.Color.Black;
+                     break;
+                 case 2:
+                     randomColor = Bunny.Color.Brown;
+                     break;
+                 case 3:
+                     randomColor = Bunny.Color.Spotted;
+                     break;
+                 case 4:
+                 default:
+                     randomColor = Bunny.Color.White;
+                     break;
+             }
+             //Create the bunny with the random color
+             bunny = new Bunny(randomColor);
+ 
+             //[Male/Female] [Black/Brown/Spotted/White] Bunny [Name] Was born!
+             Console.WriteLine("{0} {1} Bunny {2} Was born!", bunny.getSex(), bunny.getColor(), bunny.GetName());
+             return new Bunny(randomColor);
+         }
+     }
+ }

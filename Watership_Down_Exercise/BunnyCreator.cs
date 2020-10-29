629f27419690b915f3a56b1acae7418ed84c0992
shay slonim <shayslonim@gmail.com>
2020-10-29 19:18:31 +0200

    Added another method that allowes to choose the bunny's sex. Moved the Color randomization to a separate function.

--- Watership_Down_Exercise/BunnyCreator.cs
+++ Watership_Down_Exercise/BunnyCreator.cs
@@ -19,9 +19,36 @@ namespace Watership_Down_Exercise
         {
             Bunny bunny;
             Color randomColor;
+
+            //Create the bunny with the random color
+            randomColor = GenerateRandomBunnyColor();
+            bunny = new Bunny(randomColor);
+
+            //[Male/Female] [Black/Brown/Spotted/White] Bunny [Name] Was born!
+            Console.WriteLine(bunny.BunnySex + " " + bunny.BunnyColor + " Bunny " + bunny.BunnyName + " Was born!");
+            return bunny;
+        }
+
+        public static Bunny CreateBunny(Sex bunnySex)
+        {
+            Bunny bunny;
+            Color randomColor;
+
+            //Create the bunny with the random color
+            randomColor = GenerateRandomBunnyColor();
+            bunny = new Bunny(bunnySex, randomColor);
+
+            //[Male/Female] [Black/Brown/Spotted/White] Bunny [Name] Was born!
+            Console.WriteLine(bunny.BunnySex + " " + bunny.BunnyColor + " Bunny " + bunny.BunnyName + " Was born!");
+            return bunny;
+        }
+
+        private static Color GenerateRandomBunnyColor()
+        {
+            Color randomColor;
             //Get a random number between 1 and 4
             int randomNumber = random.Next(AMOUNT_OF_COLORS);
-
+            
             //Generate a random color
             switch (randomNumber)
             {
@@ -39,12 +66,7 @@ namespace Watership_Down_Exercise
                     randomColor = Color.White;
                     break;
             }
-            //Create the bunny with the random color
-            bunny = new Bunny(randomColor);
-
-            //[Male/Female] [Black/Brown/Spotted/White] Bunny [Name] Was born!
-            Console.WriteLine(bunny.BunnySex + " " + bunny.BunnyColor + " Bunny " + bunny.BunnyName + " Was born!");
-            return bunny;
+            return randomColor;
         }
     }
 }
